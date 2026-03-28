467271865a7ab4d5160f4e394440bc7555d98b57
tanuja-t <tanujathite26@gmail.com>
2026-03-28 11:24:34 +0530

    Refactor server.js and update route responses

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const Contact = require("./Models/Contact");
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
 
 app.get('/signup', (req, res) => {
